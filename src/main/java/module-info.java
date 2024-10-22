99c552d14d1f1b8ad75500c7bc5d1cc25925896b
GuiDias123 <164422098+GuiDias123@users.noreply.github.com>
2024-10-22 19:54:18 -0300

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module org.syslocacar {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires com.almasb.fxgl.all;
+
+    opens org.syslocacar to javafx.fxml;
+    exports org.syslocacar;
+}
